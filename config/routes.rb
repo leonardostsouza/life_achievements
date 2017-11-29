2c5ee5727a59f7657b803ceb9983316d954a1497
Leonardo Souza <leonardostsouza@gmail.com>
2017-11-29 19:31:07 -0200

    Change Home and Help page contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
-
   resources :user_achievements
   resources :achievements
   resources :users
-	#root 'application#hello'
   root 'users#index'
 end
