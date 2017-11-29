b0179e116be85ce59464f1d6fd7985fc53146aa6
Leonardo Souza <leonardostsouza@gmail.com>
2017-11-29 20:36:51 -0200

    Completed basic static pages behaviour

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-
-  def hello
-  	render html: "Application - Hello"
-  end
-
-  def bye
-  	render html: "Application - Bye"
-  end
 end
